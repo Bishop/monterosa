2fdde86db6897b5a964df0370bd531131a3884ea
Alexandr Prudnikov <al.prudnikov@gmail.com>
2011-10-04 15:28:59 +0300

    Loop

--- scripts/cron.php
+++ scripts/cron.php
@@ -6,6 +6,7 @@ define('STATUS_BAD_MEMCACHE', 1);
 define('STATUS_BAD_MYSQL', 2);
 define('STATUS_NO_DATA', 3);
 define('STATUS_OK', 4);
+define('STATUS_ALREADY_RUNNING', 5);
 
 function terminate($code)
 {
@@ -14,6 +15,7 @@ function terminate($code)
 		STATUS_BAD_MYSQL => 'MySQL Error',
 		STATUS_NO_DATA => 'No Data',
 		STATUS_OK => 'Success',
+		STATUS_ALREADY_RUNNING => 'Already Running',
 	);
 
 	if ($f = fopen(__DIR__ . '/../var/log/cron.log', 'ab')) {
@@ -23,38 +25,45 @@ function terminate($code)
 	exit();
 }
 
+$pid_file = __DIR__ . '/../var/run/cron.pid';
+file_exists($pid_file) and terminate(STATUS_ALREADY_RUNNING);
+
+fclose(fopen($pid_file, 'w'));
+register_shutdown_function(function() use ($pid_file) { unlink($pid_file); });
+
 $mc = memcache_connect(MC_HOST, MC_PORT) or terminate(STATUS_BAD_MEMCACHE);
 $pop = 1;
 
 memcache_add($mc, 'pop', $pop, false);
 
-$pop = memcache_get($mc, 'pop') or terminate(STATUS_BAD_MEMCACHE);
-$put = memcache_get($mc, 'put') or terminate(STATUS_BAD_MEMCACHE);
-memcache_set($mc, 'pop', $put + 1) or terminate(STATUS_BAD_MEMCACHE);
+while (true) {
+	$pop = memcache_get($mc, 'pop') or terminate(STATUS_BAD_MEMCACHE);
+	$put = memcache_get($mc, 'put') or terminate(STATUS_BAD_MEMCACHE);
+	memcache_set($mc, 'pop', $put + 1) or terminate(STATUS_BAD_MEMCACHE);
 
-$put >= $pop or terminate(STATUS_NO_DATA);
+	$put >= $pop or terminate(STATUS_NO_DATA);
 
-$data = array();
+	$data = array();
 
-for ($i = $pop; $i <= $put; $i++) {
-	$data[] = memcache_get($mc, $i);
-	memcache_delete($mc, $i);
-}
-
-(mysql_connect(DB_HOST . ':' . DB_PORT, DB_USER, DB_PASS)
-	&& mysql_select_db(DB_NAME)) or terminate(STATUS_BAD_MYSQL);
-
-$chunk_size = 100;
-foreach (array_chunk($data, $chunk_size) as $chunk) {
-	$values = array();
-	$a_len = count($chunk);
-	for ($i = 0; $i < $a_len; ++$i) {
-		$values[] = sprintf('(%s, \'%s\')', $chunk[$i][0], mysql_real_escape_string($chunk[$i][1]));
+	for ($i = $pop; $i <= $put; $i++) {
+		$data[] = memcache_get($mc, $i);
+		memcache_delete($mc, $i);
 	}
 
-	$query = 'insert emails (id, email) values ' . implode(', ', $values) . ' on duplicate key update email=values(email)';
+	(mysql_connect(DB_HOST . ':' . DB_PORT, DB_USER, DB_PASS)
+		&& mysql_select_db(DB_NAME)) or terminate(STATUS_BAD_MYSQL);
 
-	mysql_query($query) or terminate(STATUS_BAD_MYSQL);
+	$chunk_size = 100;
+	foreach (array_chunk($data, $chunk_size) as $chunk) {
+		$values = array();
+		$a_len = count($chunk);
+		for ($i = 0; $i < $a_len; ++$i) {
+			$values[] = sprintf('(%s, \'%s\')', $chunk[$i][0], mysql_real_escape_string($chunk[$i][1]));
+		}
+
+		$query = 'insert emails (id, email) values ' . implode(', ', $values) . ' on duplicate key update email=values(email)';
+
+		mysql_query($query) or terminate(STATUS_BAD_MYSQL);
+	}
 }
-
 terminate(STATUS_OK);
